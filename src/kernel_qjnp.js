fb790c81e9ff4110a3e2ab4a43595f1b3de3114b
xzcvzxc2 <rwyonr0kb1@mail10p.com>
2026-01-02 10:49:55 +0700

    docs: update installation guide

--- src/kernel_qjnp.js
+++ src/kernel_qjnp.js
@@ -1,2 +1,6 @@
 const config = { version: "3967", mode: "production" };
 module.exports = config;
+
+// Update lohr
+const config = { version: "3665", mode: "production" };
+module.exports = config;
